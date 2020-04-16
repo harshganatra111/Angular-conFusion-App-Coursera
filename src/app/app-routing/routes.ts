c26a800c5866ec0aaa0dd29835ca49b0621b9d9f
Harsh Ganatra <harshganatra111@gmail.com>
2020-04-16 21:44:57 +0530

    warnings removed

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -4,7 +4,6 @@ import { MenuComponent } from '../menu/menu.component';
 import { HomeComponent } from '../home/home.component';
 import { AboutComponent } from '../about/about.component';
 import { ContactComponent } from '../contact/contact.component';
-//import { DishdetailComponent } from '../dishdetail/dishdetail.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { AppRoutingModule  } from './app-routing/app-routing.module'
+import { AppRoutingModule  } from './app-routing/app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
